5fde569c77f6a7b8774f9155e700280066ab3060
Alex Lee <alex.lee@awtechconsulting.com>
2024-12-06 11:26:31 +0100

    Day 6

--- internal/day6/day6_test.go
+++ internal/day6/day6_test.go
@@ -1,25 +1,25 @@
-package day5
+package day6
 
 import "testing"
 
-func TestDay5Part1(t *testing.T) {
+func TestDay6Part1(t *testing.T) {
 	got, err := Execute("data/input.txt", 1)
 	if err != nil {
 		t.Error(err)
 	}
-	wanted := 143
+	wanted := 41
 
 	if got != wanted {
 		t.Errorf("got %d, but wanted %d", got, wanted)
 	}
 }
 
-func TestDay5Part2(t *testing.T) {
+func TestDay6Part2(t *testing.T) {
 	got, err := Execute("data/input.txt", 2)
 	if err != nil {
 		t.Error(err)
 	}
-	wanted := 123
+	wanted := 6
 
 	if got != wanted {
 		t.Errorf("got %d, but wanted %d", got, wanted)
